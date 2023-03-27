8780291b32ca5a27255f6e260738695d93b759ce
Bhavika Tibrewal <bhtibrewal@gmail.com>
2023-03-27 08:56:20 +0000

    correct &apos;

--- components/AboutMe/About.tsx
+++ components/AboutMe/About.tsx
@@ -12,12 +12,12 @@ const About: FC = () => {
           <h2 className="section__header-main-head">About Me</h2>
         </header>
         <p>
-          I'm a <strong>Frontend Developer</strong> whose whole interest is
+          I&apos;m a <strong>Frontend Developer</strong> whose whole interest is
           building interactive, accessible, and responsive websites. My project
           section contains some beautiful websites I&apos;ve built.
         </p>
         <p>
-          I'm currently looking into new opportunities where I can contribute,
+          I&apos;m currently looking into new opportunities where I can contribute,
           aid in business growth, and affect positive change. Contact me if you
           have a challenge for me.
         </p>
