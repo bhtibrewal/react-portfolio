22a6665c24e76dc41ba3dee96b792599729a33aa
Bhavika Tibrewal <bhtibrewal@gmail.com>
2023-04-13 19:23:49 +0000

    fix media query

--- components/Style/StyledSideNav.ts
+++ components/Style/StyledSideNav.ts
@@ -14,7 +14,7 @@ export const StyledSideNav = styled.div`
   left: 40px;
   z-index: 2;
 
-  @media screen and (max-width: 420px) {
+  @media screen and (max-width: 768px) {
     display: none;
   }
 
